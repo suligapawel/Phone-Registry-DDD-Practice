b96b4c23056e74590482e759c24a5d8d43a9dd10
Paweł Suliga <psul00@me.pl>
2020-09-27 12:02:33 +0200

    Disposal of assortment

---
+++ PhoneRegistryDDD.UtilizationTests/ProductToDisposeTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using PhoneRegistryDDD.Utilization.Entities;
+using System;
+
+namespace PhoneRegistryDDD.UtilizationTests
+{
+    internal class ProductToDisposeTests
+    {
+        [Test]
+        public void When_DisposeProduct_then_DisposeThisProduct()
+        {
+            ProductToDispose Product = ProductToDispose.Free(AnyGuid());
+
+            bool result = Product.Utilize();
+
+            Assert.That(result, Is.True);
+            Assert.That(Product.Disposed(), Is.True);
+        }
+
+        [Test]
+        public void When_TryDisposeDisposedProductAlready_then_CantDisposeThisProductAgain()
+        {
+            ProductToDispose Product = ProductToDispose.Disposed(AnyGuid());
+
+            bool result = Product.Utilize();
+
+            Assert.That(result, Is.False);
+            Assert.That(Product.Disposed(), Is.True);
+        }
+
+        [Test]
+        public void When_TryDisposePurchasedProductAlready_then_CantDisposeThisProduct()
+        {
+            ProductToDispose Product = ProductToDispose.Purchased(AnyGuid());
+
+            bool result = Product.Utilize();
+
+            Assert.That(result, Is.False);
+            Assert.That(Product.Disposed(), Is.False);
+        }
+
+        [Test]
+        public void When_TryDisposeUseddProductAlready_then_CantDisposeThisProduct()
+        {
+            ProductToDispose Product = ProductToDispose.Used(AnyGuid());
+
+            bool result = Product.Utilize();
+
+            Assert.That(result, Is.False);
+            Assert.That(Product.Disposed(), Is.False);
+        }
+
+        public Guid AnyGuid() => Guid.NewGuid();
+    }
+}
