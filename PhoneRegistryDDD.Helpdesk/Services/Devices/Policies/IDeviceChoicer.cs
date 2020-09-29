26b4041b27652d8172afaddfafd64c6325c83fce
Paweł Suliga <psul00@me.pl>
2020-09-29 16:28:17 +0200

    Service to choice the device

---
+++ PhoneRegistryDDD.Helpdesk/Services/Devices/Policies/IDeviceChoicer.cs
@@ -0,0 +1,9 @@
+﻿using PhoneRegistryDDD.Helpdesk.Dictionaries;
+
+namespace PhoneRegistryDDD.Helpdesk.Services.Devices.Policies
+{
+    public interface IDeviceChoicer
+    {
+        DeviceClass Choice();
+    }
+}
---
+++ PhoneRegistryDDD.Helpdesk/Services/Devices/Policies/SmartphoneChoicer.cs
@@ -0,0 +1,12 @@
+﻿using PhoneRegistryDDD.Helpdesk.Dictionaries;
+
+namespace PhoneRegistryDDD.Helpdesk.Services.Devices.Policies
+{
+    public class SmartphoneChoicer : IDeviceChoicer
+    {
+        public DeviceClass Choice()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
