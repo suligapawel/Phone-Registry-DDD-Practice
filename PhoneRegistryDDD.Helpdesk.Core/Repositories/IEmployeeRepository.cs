0320ecf37e801a3a656e132553ed2198e17328a3
Paweł Suliga <psul00@me.pl>
2020-10-05 20:29:43 +0200

    Repositories

---
+++ PhoneRegistryDDD.Helpdesk.Core/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using PhoneRegistryDDD.Helpdesk.Core.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace PhoneRegistryDDD.Helpdesk.Core.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<Employee> GetBy(Guid id);
+        Task<bool> Update(Employee employee);
+    }
+}
