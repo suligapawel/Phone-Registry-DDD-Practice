3fd8a329572770da59071eba967cb7fbaba8ef1a
Paweł Suliga <psul00@me.pl>
2020-11-02 21:49:11 +0100

    Add API

---
+++ PhoneRegistryDDD.API/Controllers/KitController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneRegistryDDD.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KitController : ControllerBase
+    {      
+    }
+}
