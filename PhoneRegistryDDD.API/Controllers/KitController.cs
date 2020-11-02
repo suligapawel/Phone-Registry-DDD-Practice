348041facdc989d845e148f1af464d5e7f2fbf03
Paweł Suliga <psul00@me.pl>
2020-11-02 22:52:08 +0100

    Post method to take back kit

--- PhoneRegistryDDD.API/Controllers/KitController.cs
+++ PhoneRegistryDDD.API/Controllers/KitController.cs
@@ -1,10 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PhoneRegistryDDD.Helpdesk.Core.Commands;
+using System.Threading.Tasks;
 
 namespace PhoneRegistryDDD.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class KitController : ControllerBase
-    {      
+    {
+        private readonly IMediator _mediator;
+
+        public KitController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> TakeBack(TakeBackKitCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
     }
 }
