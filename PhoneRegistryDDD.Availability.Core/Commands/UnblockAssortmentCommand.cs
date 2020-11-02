8b8566656ed0c1513de96e90021d5b82a5973923
Paweł Suliga <psul00@me.pl>
2020-11-02 22:50:43 +0100

    Unblock assortment handler

---
+++ PhoneRegistryDDD.Availability.Core/Commands/UnblockAssortmentCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace PhoneRegistryDDD.Availability.Core.Commands
+{
+    public class UnblockAssortmentCommand : INotification
+    {
+        public IEnumerable<Guid> Ids { get; }
+
+        public UnblockAssortmentCommand(IEnumerable<Guid> ids)
+        {
+            Ids = ids;
+        }
+    }
+}
---
+++ PhoneRegistryDDD.Availability.Infrastructure/Handlers/UnblockAssortmentHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using PhoneRegistryDDD.Availability.Core.Commands;
+using PhoneRegistryDDD.Availability.Core.Entities;
+using PhoneRegistryDDD.Availability.Core.Repositories;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhoneRegistryDDD.Availability.Infrastructure.Handlers
+{
+    public class UnblockAssortmentHandler : INotificationHandler<UnblockAssortmentCommand>
+    {
+        private readonly IAssortmentRepository _assortmentRepo;
+
+        public UnblockAssortmentHandler(IAssortmentRepository assortmentRepository)
+        {
+            _assortmentRepo = assortmentRepository;
+        }
+
+        public async Task Handle(UnblockAssortmentCommand notification, CancellationToken cancellationToken)
+        {
+            IEnumerable<Assortment> assortments = await _assortmentRepo.GetFewBy(notification.Ids);
+
+            foreach (var assortment in assortments)
+            {
+                assortment.Unblock();
+            }
+
+            await _assortmentRepo.UpdateFew(assortments);
+        }
+    }
+}
