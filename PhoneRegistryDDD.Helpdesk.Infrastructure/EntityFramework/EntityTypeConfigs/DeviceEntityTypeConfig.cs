0ed1f64a20ff65cda315b791867c32b354fa9709
Paweł Suliga <psul00@me.pl>
2020-12-03 00:17:16 +0100

    Add EF

---
+++ PhoneRegistryDDD.Helpdesk.Infrastructure/EntityFramework/EntityTypeConfigs/DeviceEntityTypeConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PhoneRegistryDDD.Helpdesk.Core.Entities.Devices;
+
+namespace PhoneRegistryDDD.Helpdesk.Infrastructure.EntityFramework.EntityTypeConfigs
+{
+    internal class DeviceEntityTypeConfig : IEntityTypeConfiguration<Device>
+    {
+        public void Configure(EntityTypeBuilder<Device> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+
+    internal class SmartphoneEntityTypeConfig : DeviceEntityTypeConfig
+    {
+        public void Configure(EntityTypeBuilder<Smartphone> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.ToTable("Smartphones", "helpdesk");
+        }
+    }
+}
