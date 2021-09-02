9bcdc6da0d0e16bea30e57a41450a640c13580cb
Paweł Suliga <pawel.suliga.dev@gmail.com>
2021-09-02 21:16:49 +0200

    Add projects

---
+++ PhoneRegistryDDD.Availability.Api/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhoneRegistryDDD.Availability.Application;
+using PhoneRegistryDDD.Availability.Infrastructure;
+
+[assembly: InternalsVisibleTo("PhoneRegistryDDD.API")]
+namespace PhoneRegistryDDD.Availability.Api
+{
+    internal static class DependencyInjection
+    {
+        public static IServiceCollection AddAvailability(this IServiceCollection services, IConfiguration config)
+        {
+            return services
+                .AddApplication(config)
+                .AddInfrastructure(config);
+        }
+    }
+}
---
+++ PhoneRegistryDDD.Availability.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhoneRegistryDDD.Availability.Application.Handlers;
+using PhoneRegistryDDD.Availability.Core.Commands;
+using PhoneRegistryDDD.Shared.Abstractions.Commands;
+
+[assembly: InternalsVisibleTo("PhoneRegistryDDD.Availability.Api")]
+namespace PhoneRegistryDDD.Availability.Application
+{
+    internal static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration config)
+        {
+            return services.AddScoped<ICommandHandler<UnblockAssortmentCommand>, UnblockAssortmentHandler>();
+        }
+    }
+}
